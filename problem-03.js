1098d5104794ce196ac5c5a80752b6e9b2d5e68c
faysalahmed915 <faysalahmed915@gmail.com>
2025-02-18 04:42:48 +0600

    problem-01 is done and others are being analyzed

---
+++ problem-03.js
@@ -0,0 +1,16 @@
+// problem-03
+/*function signature/sample */
+
+function  electionResult(votes) {
+    if (votes.isArray) {
+        console.log('valid');
+    }
+    else {
+        console.log('Invalid');
+    }
+}
+
+// voteArray = ["mango", "banana", "mango", "banana", "mango","mango", "banana", "Na-Vote", "mango","mango", "banana", "jaker party", "no","mango", "BananA", "na vote", "na vote"];
+
+voteResult = []
+electionResult(voteResult);
---
+++ problem-05.js
@@ -0,0 +1,17 @@
+// problem-05
+
+
+function  calculateWatchTime( times ) {
+    if (times.isArray) {
+        console.log('valid');
+    }
+    else {
+        console.log('Invalid');
+    }
+}
+
+// watchTime = [ 2, 3, 45, 5, 6, 70, 8, 19, 10, 151, 123 ];
+
+watchTime = [];
+
+calculateWatchTime(watchTime);
